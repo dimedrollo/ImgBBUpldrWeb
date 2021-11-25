f74ffa42e090a969728d7a6c92126fa707b77f8b
dimedrollo <bac9poopkin@gmail.com>
2021-11-26 00:01:45 +0300

    add Homework for lesson 5

---
+++ Hometask5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Hometask5 {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int a = scanner.nextInt();
+	int minDigit = 10;
+
+	while (a != -1){
+	    while (a > 0){
+	       int b = a % 10;
+	       a = a / 10;
+	       if (b < minDigit) minDigit = b;
+        }
+		a = scanner.nextInt();
+    }
+	System.out.println(minDigit);
+    }
+}
+
