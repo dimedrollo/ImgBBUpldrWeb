1f5909958698ec4292f32485a6d7527fad9d5076
dimedrollo <bac9poopkin@gmail.com>
2021-12-01 17:08:46 +0300

    Fixed mistakes

--- Hometask5.java
+++ Hometask5.java
@@ -1,21 +1,23 @@
+package ru.dimedrollo;
+
 import java.util.Scanner;
 
 class Hometask5 {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	int a = scanner.nextInt();
-	int minDigit = 10;
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int minDigit = 10;
 
-	while (a != -1){
-	    while (a > 0){
-	       int b = a % 10;
-	       a = a / 10;
-	       if (b < minDigit) minDigit = b;
+        while (a != -1) {
+            while (a != 0) {
+                int b = a % 10;
+                a = a / 10;
+                if (b < minDigit) minDigit = b;
+            }
+            a = scanner.nextInt();
         }
-		a = scanner.nextInt();
-    }
-	System.out.println(minDigit);
+        System.out.println(minDigit);
     }
 }
 
