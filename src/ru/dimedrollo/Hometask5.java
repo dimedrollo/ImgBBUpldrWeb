799b24e9e1647b76fe9996e59993d491320a4427
dimedrollo <bac9poopkin@gmail.com>
2021-12-02 14:03:25 +0300

    Исправлено поведение для отрицательных чисел и обработка сигнала окончания в начале ввода

--- src/ru/dimedrollo/Hometask5.java
+++ src/ru/dimedrollo/Hometask5.java
@@ -7,17 +7,23 @@ class Hometask5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int minDigit = 10;
-
-        while (a != -1) {
-            while (a != 0) {
-                int b = a % 10;
-                a = a / 10;
-                if (b < minDigit) minDigit = b;
+        if (a == -1) {
+            System.out.println("Program exit");
+        } else {
+            int minDigit = 10;
+            a = Math.abs(a);
+            while (a != -1) {
+                while (a != 0) {
+                    int b = a % 10;
+                    a = a / 10;
+                    if (b < minDigit) {
+                        minDigit = b;
+                    }
+                }
+                a = scanner.nextInt();
             }
-            a = scanner.nextInt();
+            System.out.println(minDigit);
         }
-        System.out.println(minDigit);
     }
 }
 
