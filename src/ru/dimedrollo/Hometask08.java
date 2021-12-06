33b439c1fdd2747d9e2b59a226131d4a5a3d4cef
Dimedrollo <bac9poopkin@gmail.com>
2021-12-06 14:52:03 +0300

    Fixed all issues, add defense of incorrect input, translate all in eng.

--- src/ru/dimedrollo/Hometask08.java
+++ src/ru/dimedrollo/Hometask08.java
@@ -6,64 +6,56 @@ import java.util.*;
 
 public class Hometask08 {
 
-    //Создаем класс Person реализующий интерфейс Comparable
+    //Creating a Person class that implements the Comparable interface
     public static class Person implements Comparable<Person> {
-        String name;
-        Integer weight;
+        private String name;
+        private Float weight;
 
-        //Добавляем конструктор класса
-        public Person(String name, Integer weight) {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Float getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Float weight) {
+            this.weight = weight;
+        }
+
+        // Add a constructor
+        public Person(String name, Float weight) {
             this.name = name;
             this.weight = weight;
         }
 
-        //Добавляем реализацию метода интерфейса - возможность сравнения объектов Person по параметру weight
+        //Adding the implementation of the interface method - the ability to compare Person objects by the weight parameter
         @Override
         public int compareTo(Person h) {
-            return weight.compareTo(h.weight);
+            return weight.compareTo(h.getWeight());
         }
     }
 
     public static void main(String[] args) {
-        //Создаем массив на 10 элементов и сканер для чтения данных с консоли
+        //Creating an array of 10 elements and a scanner to read data from the input
         Person[] personArray = new Person[10];
         Scanner scanner = new Scanner(System.in);
-        //Заполняем массив объектами Person
+        //Filling the array with Person objects
         for (int i = 0; i < personArray.length; i++) {
-        //В конструктор передаются строка и число с консоли
-            personArray[i] = new Person(scanner.next(), scanner.nextInt());
+            //If incorrect data is being entered, we replace it with zero
+            personArray[i] = new Person(scanner.next(), scanner.hasNextFloat() ? scanner.nextFloat() : 0);
         }
-        //Сортируем массив элементов
+        //Sorting an array of Person objects
         Arrays.sort(personArray);
-        //Выводим построчно отсортированный массив объектов
+        //Output a line-by-line sorted array of objects
         for (Person p : personArray) {
-            System.out.println(p.name + " " + p.weight);
+            System.out.println(p.getName() + " " + p.getWeight());
         }
     }
 }
-        /*
-        Вводные данные для проверки:
 
-        human0
-        12
-        human1
-        55
-        human2
-        44
-        human3
-        91
-        human4
-        65
-        human5
-        43
-        human6
-        51
-        human7
-        77
-        human8
-        69
-        human9
-        100
-
-        */
 
